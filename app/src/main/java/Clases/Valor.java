2f20ed28ef594626b09e42cd70ac2fd851961ead
Igor Llancapan <55776451+igONELL@users.noreply.github.com>
2020-10-12 15:02:14 -0300

    Commit inicial

---
+++ app/src/main/java/Clases/Valor.java
@@ -0,0 +1,32 @@
+package Clases;
+
+public class Valor {
+
+//Clases para commit inicial
+
+    private int valorFarenheith;
+    private int valorRevival;
+    private int valorElalquimista;
+
+    public Valor ()
+    {
+
+        valorFarenheith = 5000;
+        valorRevival = 12000;
+        valorElalquimista =  45000;
+
+    }
+
+    public int getValorFarenheith() {
+        return valorFarenheith;
+    }
+
+    public int getValorRevival() {
+        return valorRevival;
+    }
+
+    public int getValorElalquimista() {
+        return valorElalquimista;
+    }
+}
+
---
+++ app/src/main/java/com/example/app_biblioteca/Home_act.java
@@ -0,0 +1,32 @@
+package com.example.app_biblioteca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
+
+public class Home_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_act);
+    }
+
+    public void Libro(View v)
+    {
+        ArrayList<String> libros = new ArrayList<String>();
+
+        libros.add("Revival");
+        libros.add("Farenheith");
+        libros.add("El Alquimista");
+
+        Intent i = new Intent(this, Github_act.class);
+        i.putExtra("ListaLibros", libros);
+        startActivity(i);
+    }
+}
